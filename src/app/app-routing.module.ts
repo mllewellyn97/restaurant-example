d09b2605bd634f31c1606932a1221c6676f0d207
Macayla Llewellyn <macayla1997@outlook.com>
2025-09-01 22:28:55 -0400

    Add careers component and footer component, fix width on homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'menu', component: MenuComponent},
   { path: 'contact', component: ContactComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'careers', component: CareersComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { CareersComponent } from './careers/careers.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ContactComponent } from './contact/contact.component';
     HomeComponent,
     MenuComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    CareersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
