50da8e34d868099118c8ef6dc859fb6ac126bb45
Macayla Llewellyn <macayla1997@outlook.com>
2025-09-03 20:51:13 -0400

    Font style change and minor modifications to webpage

--- src/app/food-item-carousel/food-item-carousel.component.ts
+++ src/app/food-item-carousel/food-item-carousel.component.ts
@@ -8,12 +8,17 @@ import { Component } from '@angular/core';
 export class FoodItemCarouselComponent {
 images = ['assets/double-deluxe-shrimp.jpg', 'assets/sushi.jpg', 'assets/clams.jpg'];
 currentIndex = 0;
-
+nextIndex = this.currentIndex +1;
+nextNextIndex = this.nextIndex +1;
 next() {
   this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  this.nextIndex = this.currentIndex + 1;
+  this.nextNextIndex = this.nextIndex +1;
 }
 prev() {
   this.currentIndex =
     (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.nextIndex = this.currentIndex - 1;
+      this.nextNextIndex = this.nextIndex -1;
 }
 }
